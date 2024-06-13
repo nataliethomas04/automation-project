fb3867fb76f6bc344637cd6cba9d66e0b7e530e9
Natalie Thomas <nataliethomas@Natalies-MBP.lan>
2024-06-13 10:32:08 -0400

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region     = "us-east-1"  # Specify your desired AWS region
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
+resource "aws_instance" "example" {
+  ami                    = "ami-0e731c8a588258d0d" 
+  instance_type          = "t2.micro"
+  key_name               = "terraform-key-pairs"
+  vpc_security_group_ids = ["sg-0d6473f814374a9a6"]
+  tags = {
+    Name = "react project"
+
+
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "mkdir project",
+      "cd project",
+      "sudo yum install git -y",
+      "git clone https://github.com/nataliethomas04/my-react-app.git",
+      "cd react-aws-terraform-project",
+      "sudo yum install -y nodejs npm",
+      "node --version",
+      "npm --version",
+      "npm install web-vitals",
+      "npm install react-scripts --save-dev",
+      "npm install react-dom",
+      "npm install react-router-dom",
+      "cd ~/automation-project",
+      "npm start"
+      # Additional commands to start your application
+
+    ]
+    
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = base64decode(var.private_key_base64)
+      host        = self.public_ip
+    }
+  }
+}
+
+variable "private_key_base64" {
+  description = "Base64 encoded private key content"
+  type        = string
+}
+
+variable "aws_access_key_id" {
+  description = "AWS access key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret access key"
+  type        = string
+}
