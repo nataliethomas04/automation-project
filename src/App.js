5c444566342f84299d3ab2b1827894734fdece51
Natalie Thomas <nataliethomas@Natalies-MBP.lan>
2024-06-18 11:46:40 -0400

    hello

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hello Guyss
+        Hello Guys
         </p>
         <a
           className="App-link"
