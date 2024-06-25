c91f6f2484cd3a54ae93d7f063014321fefb4871
Natalie Thomas <nataliethomas@Natalies-MBP.lan>
2024-06-25 11:26:46 -0400

    changing cd

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hellos Worldss
-        </p>
+        Helloing Worlding
+</p>
         <a
           className="App-link"
           href="https://reactjs.org"
