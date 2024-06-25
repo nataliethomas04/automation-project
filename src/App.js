626da51db41eb4d70753e1189154cd6a57685ed1
Natalie Thomas <nataliethomas@Natalies-MBP.lan>
2024-06-25 11:45:19 -0400

    send help

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Helloing Worlding
+        Test Test Testing
 </p>
         <a
           className="App-link"
