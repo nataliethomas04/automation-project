0c0ed23160df9d5da0ffc2210f0de6a627f6b4ee
Natalie Thomas <nataliethomas@Natalies-MBP.lan>
2024-06-25 11:06:36 -0400

    change key

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        Hellos Worlds
+        Hellos Worldss
         </p>
         <a
           className="App-link"
