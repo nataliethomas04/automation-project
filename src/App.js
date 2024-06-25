4fd4bdb7c4967d7db10aae3e0f2dba84339d6e31
Natalie Thomas <nataliethomas@Natalies-MBP.lan>
2024-06-25 13:03:25 -0400

    testing deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Jonny rocks the socks off my locks 
+            POOOOOP 
         </p>
         <a
           className="App-link"
