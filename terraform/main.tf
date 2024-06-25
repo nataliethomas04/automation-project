6b71b5f75412097ceca4670ca9c7b9acdd6b026f
Natalie Thomas <nataliethomas@Natalies-MBP.lan>
2024-06-25 09:38:07 -0400

    test run

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,7 @@ resource "aws_instance" "example" {
   key_name               = "key-pair"
   vpc_security_group_ids = ["sg-04e3dbe8807de2c50"]
   tags = {
-    Name = "yessssss"
-
+    Name = "Test Run"
 
   }
   
