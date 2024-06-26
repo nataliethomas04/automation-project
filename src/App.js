1538f149fbf4115a8d6e73145c9cb318692d9268
Natalie Thomas <nataliethomas@Natalies-MBP.lan>
2024-06-26 10:49:20 -0400

    testing deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
      <header className="App-header">
        <img src={logo} className="App-logo" alt="logo" />
        <p>
-         Hello World
+         It's Wednesday June 26th
        </p>
        <a
          className="App-link"
