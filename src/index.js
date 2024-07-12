1f8a7fd674d2aa5d457676e899558174ae2b28b8
Natalie Thomas <nataliethomas@Natalies-MacBook-Pro.local>
2024-07-12 08:34:59 -0400

    Student Registration APP

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -9,3 +10,5 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+reportWebVitals();
